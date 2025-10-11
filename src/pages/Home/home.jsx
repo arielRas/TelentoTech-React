b6a48f1db2ee3d8097bf5ee8cdbde8c9209e28bd
arielRas <ariel.ras.90@gmail.com>
2025-10-11 01:28:29 -0300

    Se agrega imagen central y h1 en la page Home.

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,14 +1,23 @@
-import {ProductList} from "../../containers/ProductList/Productlist";
-import {MainLayout} from "../../layouts/MainLayout/MainLayout";
+import { ProductList } from "../../containers/ProductList/Productlist";
+import { MainLayout } from "../../layouts/MainLayout/MainLayout";
 import "./home.css";
 
 export const Home = () => {
-    return(
+    return (
         <MainLayout>
-            <div className="home-central-section-container">
-                {/* <img src="../../public/img/common/home-central-image.png" alt="" /> */}
-            </div>
-            <ProductList/>
+            <section className="home-central-section-container">
+                <div className="home-h1-container">
+                    <h1>
+                        Bienvenido a <span className="home-text-color-1">E-Shop</span><br />
+                        Tu destino en <span className="home-text-color-2">tecnología</span>
+                    </h1>
+                    <p>Encontrá los mejores productos informáticos con la mejor atención y garantía.</p>
+                </div>
+                <div className="home-central-img-container">
+                    <img src="./public/img/common/home-central-image-2.png" alt="" />
+                </div>
+            </section>
+            <ProductList />
         </MainLayout>
     );
 }
