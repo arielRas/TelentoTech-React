3ab6fe7963c89b9541a2ecf10dcf64e4948eff5d
arielRas <ariel.ras.90@gmail.com>
2025-10-18 00:27:23 -0300

    Se aplica diseño responsive en header y nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import {useCartContext} from "../../contexts/CartContext/useCartContext";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useCartContext } from "../../contexts/CartContext/useCartContext";
 import "./Header.css";
 
 export const Header = () => {
-    const {countItemsCart} = useCartContext();
+    const [isMenuActive, setIsMenuActive] = useState(false);
+    const { countItemsCart } = useCartContext();
 
     return (
         <header>
@@ -12,29 +14,39 @@ export const Header = () => {
             </div>
 
             <nav>
-                <ul>
+                <ul className={isMenuActive ? "menu-active" : "menu-inactive"}>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/">Nosotros</Link></li>
                     <li><Link to="/">Contacto</Link></li>
+                    <li className="nav-hidden-link"><Link to="/cart">Carrito</Link></li>
+                    <li className="nav-hidden-link"><Link to="/">Login</Link></li>
                 </ul>
             </nav>
 
             <div className="header-aux-container">
-                <div className="header-login-button-container">
-                    <Link to="/">
-                        <div className="link-button ">Login</div>
-                    </Link>
-                </div>
                 <div className="header-cart-button-container">
                     <Link to="/cart">
                         <div className="link-button ">
                             <span>Carrito</span>
-                            { countItemsCart() > 0 &&
+                            {countItemsCart() > 0 &&
                                 <span className="cart-item-counter">{countItemsCart()}</span>
                             }
                         </div>
                     </Link>
                 </div>
+
+                <div className="header-login-button-container">
+                    <Link to="/">
+                        <div className="link-button ">Login</div>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="burger-btn-container">
+                <button
+                    className="burger-btn"
+                    onClick={() => setIsMenuActive(!isMenuActive)}>&#9776;
+                </button>
             </div>
         </header>
     );
